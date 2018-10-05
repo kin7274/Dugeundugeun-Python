9c4741fef2a05152949786ac5a4fb950f610d028
kin7274 <kms6742@naver.com>
2018-10-06 00:20:43 +0900

    2장 연습문제 클리어
    2장 연습문제 클리어

---
+++ 2장/연습문제4.py
@@ -0,0 +1,13 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+r=50
+t.circle(r)
+t.penup()
+t.goto(100,0)
+t.pendown()
+t.circle(r+20)
+t.penup()
+t.goto(200,0)
+t.pendown()
+t.circle(r+20+20)
---
+++ 2장/연습문제5.py
@@ -0,0 +1,10 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+side=100
+t.fd(side)
+t.lt(120)
+t.fd(side)
+t.lt(120)
+t.fd(side)
+t.lt(120)
